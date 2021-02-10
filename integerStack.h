ac97ea06adb6c1dfeb56507b14cfbe06d1eea8af
Panichito <74043014+Panichito@users.noreply.github.com>
2021-02-10 22:57:46 +0700

    Add files via upload

---
+++ integerStack.h
@@ -0,0 +1,28 @@
+#ifndef INTEGERSTACK_H
+#define INTEGERSTACK_H
+#define UNDERFLOW -9999
+/**
+ * Push a data item onto the stack 
+ * Arguments:
+ *   intdata      -   integer value to be pushed  
+ * Returns 1 if successful, 0 if we have an overflow.
+ */
+int pushInt(int intdata);
+/**
+ * Removes and returns the top item on the stack.
+ * Returns special value -9999 if the queue is empty 
+ * (that is, underflow).
+ */
+int popInt();
+/**
+ *  Find out how many items are currently in the stack.
+ * Return number of items in the queue (could be zero)
+ */
+int stackSizeInt();
+/**
+ *  Clear so we can reuse 
+ */
+void stackClearInt();
+/** DEBUGGING FUNCTION PRINTS SOME INFO ABOUT THE STACK **/
+void printDebugInt();
+#endif
